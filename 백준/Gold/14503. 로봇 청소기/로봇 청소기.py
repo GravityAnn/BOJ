38d0de601db4ec2f088b65eb469665866ee52e75
jungHyeockAnn <168503880+GravityAnn@users.noreply.github.com>
2025-01-17 15:48:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,40 +1,54 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-r, c, d = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False]*M for _ in range(N)]
+n, m = map(int, input().split())
+x, y, d = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visit = [[False] * m for _ in range(n)]
+cnt = 0
+action = True
 
-# 방향: 0북, 1동, 2남, 3서
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+def func1(x, y, d):
+    global cnt
+    action = True
+    while action:
+        # print(x, y, cnt)
+        #1번 행동
+        if (visit[x][y] == False) and (graph[x][y] == 0):
+            #방문한적 없고, 방이면
+            visit[x][y] = True#청소
+            cnt += 1
 
-def turn_left(d):
-    return (d + 3) % 4
+        #3번 행동
+        turn = False
+        for _ in range(4):
+            d = (d+3)%4
+            nx = x + dx[d]
+            ny = y + dy[d] #일단 방향 전환하고
+            if(0 <= nx < n) and (0 <= ny < m):
+                if(visit[nx][ny] == False) and (graph[nx][ny] == 0):
+                    x = nx
+                    y = ny
+                    turn = True
+                    break#방향 전환
 
-count = 0
+        #2번 행동
+        if turn == False:#전부다 막혀있거나 이동을 안했으면,
+            nx = x - dx[d]
+            ny = y - dy[d] #후진
+            if (0 <= nx < n) and (0 <= ny < m) and (graph[nx][ny] == 0):
+                    x = nx
+                    y = ny
+            else:
+                action = False
+                break
 
-while True:
-    if not visited[r][c]:
-        visited[r][c] = True
-        count += 1
 
-    
-    moved = False
-    for _ in range(4):
-        d = turn_left(d)
-        nr, nc = r + dx[d], c + dy[d]
-        if 0 <= nr < N and 0 <= nc < M and room[nr][nc] == 0 and not visited[nr][nc]:
-            r, c = nr, nc
-            moved = True
-            break
 
-    if not moved:
-        br, bc = r - dx[d], c - dy[d]
-        if 0 <= br < N and 0 <= bc < M and room[br][bc] == 0:
-            r, c = br, bc  # 후진
-        else:
-            break 
 
-print(count)
+
+
+func1(x, y, d)
+print(cnt)
