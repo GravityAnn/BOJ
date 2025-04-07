ff75d1ba0c43aec79b393dc97ed57befa6b7b0d6
jungHyeockAnn <168503880+GravityAnn@users.noreply.github.com>
2025-04-07 18:59:39 +0900

    [Silver III] Title: 숫자 정사각형, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -1,19 +1,15 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-n, m = map(int, input().split())
-
-arr = []
-
-for i in range(n):
-    arr.append(list(input()))
-
+n, m = map(int,input().split())
+graph = [list(input()) for _ in range(n)]
 check = min(n, m)
 answer = 0
+
 for i in range(n):
     for j in range(m):
         for k in range(check):
-            if ((i + k) < n) and ((j + k) < m) and (arr[i][j] == arr[i][j + k] == arr[i + k][j] == arr[i + k][j + k]):
-                answer = max(answer, (k + 1) ** 2)
+            if (i+k < n) and (j+k < m) and (graph[i][j] == graph[i+k][j] == graph[i][j+k] == graph[i+k][j+k]):
+                answer = max(answer,k)
 
-print(answer)
+print((answer+1)**2)
