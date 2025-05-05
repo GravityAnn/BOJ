aaef1f25ef88e0a6d4bcc92f0137725dea920f37
jungHyeockAnn <168503880+GravityAnn@users.noreply.github.com>
2025-05-05 16:06:01 +0900

    [Silver IV] Title: 빙고, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,70 +1,59 @@
 import sys
 input = sys.stdin.readline
 
-#빙고
-#25개칸
-#가장 먼저 외치는 사람이 승자
-#몇번째 수를 부른 후에 철수가 빙고를 외치는지
-
+#빙고판
 list1 = [list(map(int, input().split())) for _ in range(5)]
-visit = [[0]*5 for _ in range(5)]
-list2 = [list(map(int, input().split())) for _ in range(5)]
-# print(visit)
-# print(list1)
+
+#사회자가 부르는 수
+list2 = [list(map(int,input().split())) for _ in range(5)]
 # print(list2)
-
-def check():
-    total = 0
-    for i in range(5):#가로
-        count = 0
+def check(list1):
+    bingo = 0
+    #행
+    for i in range(5):#ㅡ
+        count_row = 0
         for j in range(5):
-            if visit[i][j] == 1:
-                count += 1
-        if count == 5:
-            total += 1
+            if list1[i][j] == 'O':
+                count_row += 1
+        if count_row == 5:
+            bingo += 1
 
-    for j in range(5):#세로
-        count = 0
+    # 열
+    for j in range(5): #|
+        count_col = 0
         for i in range(5):
-            if visit[i][j] == 1:
-                count += 1
-        if count == 5:
-            total += 1
+            if list1[i][j] == 'O':
+                count_col += 1
+        if count_col == 5:
+            bingo += 1
 
-    count = 0
+    #대각선
+    count3 = 0
+    count4 = 0
     for i in range(5):
-        if visit[i][i] == 1:
-            count += 1
-    if count == 5:
-        total += 1
+        if list1[i][i] == 'O': # \
+            count3 += 1
+        if list1[i][4-i] == 'O': # /
+            count4 += 1
+    if count3 == 5:
+        bingo += 1
+    if count4 == 5:
+        bingo += 1
 
-    count = 0
-    for i in range(5):
-        if visit[i][4-i] == 1:
-            count = count + 1
-    if count == 5:
-        total += 1
-    return total
+    return bingo
 
-ans = 0
-ans_count = 0
-for i in range(5):
-    for j in range(5):
-        a = list2[i][j]
-        ans += 1
+count = 0
+for i in range(25):
+    num = list2[int(i//5)][i%5]
+    count += 1
+    for j in range(25):
+        if num == list1[int(j/5)][j%5]:
+            list1[int(j / 5)][j % 5] = 'O'
+            break
 
-        for x in range(5):
-            for y in range(5):
-                if list1[x][y] == a:
-                    visit[x][y] = 1
-        # print("ans : ",ans)
-        # for q in range(5):
-            # print(visit[q])
-        # print("현재 빙고 개수",check())
-        if check() >= 3:
-            print(ans)
-            exit()
-        # print("\n")
+    if check(list1) >= 3:  # 빙고 3줄 이상
+        print(count)
+        break
 
 
 
