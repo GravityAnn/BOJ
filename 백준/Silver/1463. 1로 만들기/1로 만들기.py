b219e56fbf861e97c2881e04421defcd2f5f368f
jungHyeockAnn <168503880+GravityAnn@users.noreply.github.com>
2025-05-18 15:05:37 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 126392 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [1e9]*(n+1)
+dp[n] = 0
+keep = True
+
+for i in range(n, 0, -1):
+    if i%3 == 0:
+        dp[i//3] = min(dp[i//3], dp[i]+1)
+    if i % 2 == 0:
+        dp[i // 2] = min(dp[i // 2], dp[i] + 1)
+    dp[i-1] = min(dp[i-1], dp[i]+1)
+print(dp[1])
+
